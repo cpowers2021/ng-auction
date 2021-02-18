9dbc5e4392e9cfe9e837338c0874a12651ee0e4c
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 18:30:05 -0800

    Feat: Create a route for the product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
-  path: '', () => import('./home/home.module').then(m => m.HomeModule)
+  path: '', () => import('./home/home.module').then(m => m.HomeModule),
+},
+{
+  path: 'products/:productId', loadChildren: () =>
+  import('./product/product.module').then(mod => mod.ProductModule)
 }];
 
 @NgModule({
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { ProductComponent } from './product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductComponent, ProductDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule.forChild([
+    {path: '', component: ProductComponent}
+    ])
   ]
 })
 export class ProductModule { }
