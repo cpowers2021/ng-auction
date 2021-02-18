f8a5ae407bdd1f3fa3f13ef665b7cf20d798aecf
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 19:05:17 -0800

    Feat: Add imports into the product module

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,12 +4,17 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import { ProductComponent } from './product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import {RouterModule} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import { ProductSuggestionComponent } from './product-suggestion/product-suggestion.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [ProductComponent, ProductDetailComponent],
+  declarations: [ProductComponent, ProductDetailComponent, ProductSuggestionComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MatGridListModule,
+    MatButtonModule,
     RouterModule.forChild([
     {path: '', component: ProductComponent}
     ])
